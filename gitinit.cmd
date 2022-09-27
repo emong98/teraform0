32acc7722728adb40d53102689c5fb088826a26a
Ed Broas <ebroas@ncu.edu>
2022-09-27 11:46:11 -0700

    teraform practice

---
+++ gitinit.cmd
@@ -0,0 +1,7 @@
+echo "# teraform0" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/emong98/teraform0.git
+git push -u origin main
